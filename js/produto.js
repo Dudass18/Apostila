a7e4e6ed1641e7a18db6152eec3cc9823c13831b
Dudass18 <162219386+Dudass18@users.noreply.github.com>
2024-10-28 20:30:33 -0300

    Add files via upload

--- js/produto.js
+++ js/produto.js
@@ -21,3 +21,25 @@ console.log(
     )
 
 console.log(empresa)
+
+
+
+
+
+
+
+
+var textoInteiro = "10";
+var inteiro = parseInt(textoInteiro);
+
+var textoFloat = "10.22";
+var float = parseFloat(textoFloat);
+console.log(
+textoInteiro,textoFloat,inteiro,float
+)
+
+var milNumber = 1000;
+var milString =  milNumber.toFixed(2);
+console.log(milString);
+
+var s1 = 
