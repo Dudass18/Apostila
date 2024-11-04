3184071c0a36f315266266329511cebd66e76d0b
Dudass18 <162219386+Dudass18@users.noreply.github.com>
2024-11-04 20:32:15 -0300

    Add files via upload

--- js/produto.js
+++ js/produto.js
@@ -42,4 +42,29 @@ var milNumber = 1000;
 var milString =  milNumber.toFixed(2);
 console.log(milString);
 
-/* var s1 =  */
+
+var s1 = "Caelum";
+var s2 = "Inovação";
+console.log(s1+s2); // imprime Caelum Inovação
+
+
+
+var num1 = 2;
+var num2 = 3;
+var nome = "Caelum";
+
+console.log(num1 + nome + num2);
+console.log(num1 + num2 + nome);
+console.log(nome + num1 + num2);
+console.log(nome + (num1 + num2));
+console.log(nome + num1 * num2);
+
+
+console.log(10-"curso");
+
+var resultado = 10-"curso"; // retorna NaN
+resultado == NaN; // false
+NaN == NaN; // true
+
+var resultado = 10-"curso";
+isNaN(resultado); // true
