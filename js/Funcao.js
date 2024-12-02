32836d7b64715b8e0ada6ad6badbb0790c352625
Dudass18 <162219386+Dudass18@users.noreply.github.com>
2024-12-02 20:28:40 -0300

    Add files via upload

--- js/Funcao.js
+++ js/Funcao.js
@@ -2,14 +2,16 @@ $("#cabecalho").css({"margin-top": "20px", "color": "#333333"})
              .addClass("selecionado");
 
 
-$('tbody td').hide();
+$('.tbody td').hide();
 
-$('#form').css('background', 'black');
+$('ul ul > li:last').css('background', 'black');
+
+$('#form')
 
 $('.headline').hide();
 
 $('p').text('alô :D');
 
-$("#menu-departamentos li").each (function (index, item) {
+$("#menu-departamentos li").each(function (index, item) {
     alert(item.text());
-});
+});                         
