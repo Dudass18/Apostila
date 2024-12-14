2aaf5306e0e8b70a17ffa7462ae6da0db802eeba
Dudass18 <162219386+Dudass18@users.noreply.github.com>
2024-12-13 21:18:11 -0300

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -6,6 +6,19 @@ $('.mais-vendidos button').click(function() {
 $('.mais-vendidos').toggleClass('painel-aberto');
 }); */
 
-$('.painel button').click(function() {
+
+/* $('.painel button').click(function() {
 $(this).parent().toggleClass('painel-aberto');
+}); */
+
+
+
+/* $('.painel button').click(function() {
+$(this).parent().toggleClass('')
+} ???????????? */
+
+
+
+$('.painel button'). click(function(){
+$('.painel button:hover').toggleClass('painel button:after')
 });
